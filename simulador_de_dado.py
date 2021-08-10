ed0699ce2c6cd475197fd55b8a1697c7d42da420
guilherme-ribeiro27 <gui062702@gmail.com>
2021-08-10 11:09:40 -0300

    Simulador de dado com tela completo

--- simulador_de_dado.py
+++ simulador_de_dado.py
@@ -1,40 +1,36 @@
 #simulador de dados
 #simular um uso de um dado o, gerando um valor de 1 até 6
 import random
-import pySimpleGUI
+import PySimpleGUI as sg
 class SimuladorDeDado:
     def __init__(self):
         self.valor_minimo = 1
         self.valor_maximo = 6
         self.mensagem = 'Você gostaria de gerar um novo valor para o dado? (s/n)'
         #Layout
-        layout = [
+        self.layout = [
             [sg.Text('Jogar o dado?')],
-            [sg.Button('Sim'),sg.Button('Não')]
+            [sg.Button('sim'),sg.Button('não')]
         ]
-        #criar uma janela
-        self.janela = sg.Window('Simulador de Dado',Layout=layout)
-        #ler os valores da tela
-        self.eventos, self.valores = janela.Read()
-        #fazer alguma coisa com os valores
+        
     
     def Iniciar(self):
         #criar uma janela
-        self.janela = sg.Window('Simulador de Dado',Layout=layout)
+        self.janela = sg.Window('Simulador de Dado', layout = self.layout)
         #ler os valores da tela
-        self.eventos, self.valores = janela.Read()
+        self.eventos, self.valores = self.janela.Read()
         #fazer alguma coisa com os valores
-        while True:
-            try:
+        
+        try:
                 
-                if  self.eventos =='s' or  self.eventos == 'sim':
-                    self.GerarValorDoDado()
-                elif  self.eventos == 'n' or  self.eventos == 'não':
-                    print("Tchau!")
-                else:
-                    print("Favor digitar s para sim ou n para não!")
-            except:
-                print("Ocorreu um erro!")
+            if  self.eventos =='s' or  self.eventos == 'sim':
+                self.GerarValorDoDado()
+            elif  self.eventos == 'n' or  self.eventos == 'não':
+                print("Tchau!")
+            else:
+                print("Favor digitar s para sim ou n para não!")
+        except:
+            print("Ocorreu um erro!")
 
     def GerarValorDoDado(self):
         print(random.randint(self.valor_minimo, self.valor_maximo))
